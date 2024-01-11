0d6d77c970c59e4aa4e545bd6df4a29a53264fe5
Shravan Divity <shravandivity@gmail.com>
2024-01-11 11:36:18 +0530

    changed master code

--- API/WebAPI/Controllers/ProductController.cs
+++ API/WebAPI/Controllers/ProductController.cs
@@ -16,7 +16,7 @@ namespace WebAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3", "value4" };
         }
 
         // GET api/values/5
